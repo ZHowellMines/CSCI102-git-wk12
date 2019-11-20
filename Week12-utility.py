c4d5a20005b7450f950469498b4169cb5f9746f2
Zachary Howell <zhowell@mines.edu>
2019-11-19 18:22:56 -0700

    created "WordCount" function

--- Week12-utility.py
+++ Week12-utility.py
@@ -18,3 +18,10 @@ def UpdateString(string1,string2,index_int):
     string1_list[index_int] = string2
     updated_string1 = ''.join(string1_list)
     print('OUTPUT', updated_string1)
+
+def FindWordCount(wc_list,wc_string):
+    word_count = 0
+    for word in wc_list:
+        if word == wc_string:
+            word_count += 1
+    return word_count
